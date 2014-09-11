e981c1095ce5a808ff97fcb44bd9c833dfe6ac0a
clicube <clicube@gmail.com>
2014-09-12 03:34:36 +0900

    board: 赤外線出力のPWM出力ON/OFFをOCR2Bでやるようにした・出力ベリファイスイッチ

--- board/irctrl.c
+++ board/irctrl.c
@@ -25,6 +25,10 @@
 #define IRCTRL_SEND_STATE_SEND_1_BIT2 7
 #define IRCTRL_SEND_STATE_SEND_1_BIT3 8
 
+//#define IRCTRL_VERIFY_OUTPUT
+#define IRCTRL_VERIFY_OK 255
+#define IRCTRL_VERIFY_NG 254
+
 
 uint8_t start_timer(uint16_t period);
 static inline void stop_timer();
@@ -34,11 +38,6 @@ static inline uint8_t start_send(uint16_t period);
 static inline void stop_send(void);
 static inline void isr_scan(void);
 static inline void isr_send(void);
-
-/* TMP */
-char out_data[256];
-uint8_t out_len;
-uint16_t out_count;
 
 struct _scan_state_t
 {
@@ -61,6 +60,12 @@ struct _send_state_t
   char* data_ptr;
   uint8_t sending_char;
   uint8_t sending_bit;
+  uint8_t OCR2B_H;
+#ifdef IRCTRL_VERIFY_OUTPUT
+  uint8_t current_operation;
+  uint8_t verify_state;
+  uint16_t sent_count;
+#endif
 };
 typedef struct _send_state_t send_state_t;
 
@@ -193,10 +198,24 @@ uint8_t ir_send(char* params_str)
   // - wait for complete sending
   while(send_state->end_flag==0); // wait for scan
 
-  /* print test data */
-  //out_data[out_len] = '\0';
-  ltoa(out_count,out_data,10);
-  dprintln(out_data);
+#ifdef IRCTRL_VERIFY_OUTPUT
+  switch(send_state->verify_state)
+  {
+    case IRCTRL_VERIFY_OK:
+      println_P("verify: OK");
+      break;
+    case IRCTRL_VERIFY_NG:
+      println_P("verify: NG");
+      print_P("sent count: ");
+      char str[6];
+      itoa(send_state->sent_count, str, 10);
+      println(str);
+      break;
+    default:
+      println_P("verify state is invalid.");
+  }
+
+#endif
 
   RETURN_CMD_OK;
 }
@@ -304,8 +323,6 @@ void stop_scan()
 
 uint8_t start_send(uint16_t period)
 {
-  out_len = 0;
-  out_count = 0;
   uint8_t ret;
 
   /* initialize vars */
@@ -318,6 +335,12 @@ uint8_t start_send(uint16_t period)
   send_state->data_ptr = send_state->data;
   send_state->sending_char = 0xFF;
   send_state->sending_bit = 0;
+
+#ifdef IRCTRL_VERIFY_OUTPUT
+  send_state->current_operation = IRCTRL_SEND_OPERATION_L;
+  send_state->verify_state = 0;
+  send_state->sent_count = 0;
+#endif
 
 
   /* calculation OCR2A ( TOP value for timer2 )
@@ -363,10 +386,13 @@ uint8_t start_send(uint16_t period)
    *
    * ==> start timer
    */
-  TCCR2A = 0b00000011;
+  //TCCR2A = 0b00000011;
+  TCCR2A = 0b00110011;
   TCCR2B = 0b00001000 | cs2n;
   OCR2A = top;
-  OCR2B = top/3;
+  send_state->OCR2B_H = top/3*2;
+  //OCR2B = top/3;
+  OCR2B = 255;
   TIMSK2 = 0x00;
 
   /* start timer */
@@ -385,7 +411,6 @@ void stop_send()
 
   /* stop carrier */
   TCCR2A &= ~0b00110000;
-
 
   send_state->end_flag = 1;
 }
@@ -454,23 +479,47 @@ void isr_scan()
 
 void isr_send()
 {
+#ifdef IRCTRL_VERIFY_OUTPUT
+  uint8_t input;
+
+  input = ((PIND & _BV(PIN7))>>PIN7) ^ 1;
+  if(
+      ( send_state->current_operation == IRCTRL_SEND_OPERATION_L && input != 0 ) ||
+      ( send_state->current_operation == IRCTRL_SEND_OPERATION_H && input != 1 )
+    )
+  {
+    send_state->verify_state = IRCTRL_VERIFY_NG;
+    stop_send();
+    return;
+  }
+  if( send_state->next_operation == IRCTRL_SEND_OPERATION_STOP )
+  {
+    send_state->verify_state = IRCTRL_VERIFY_OK;
+  }
+#endif
+
   /* change output */
   switch(send_state->next_operation)
   {
     case IRCTRL_SEND_OPERATION_L:
-      TCCR2A &= ~0b00110000;
-      //out_data[out_len] = '0'; out_len++;
-      out_count++;
+      //TCCR2A &= ~0b00110000;
+      OCR2B = 255;
       break;
     case IRCTRL_SEND_OPERATION_H:
-      TCCR2A |= 0b00100000;
-      //out_data[out_len] = '1'; out_len++;
-      out_count++;
+      //TCCR2A |= 0b00100000;
+      OCR2B = send_state->OCR2B_H;
       break;
     case IRCTRL_SEND_OPERATION_STOP:
       stop_send();
       return;
   }
+
+#ifdef IRCTRL_VERIFY_OUTPUT
+  send_state->verify_state = 1;
+  send_state->current_operation = send_state->next_operation;
+  send_state->sent_count += 1;
+#endif
+
 
   /* set next operation */
   char c;
